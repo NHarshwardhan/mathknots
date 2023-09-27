f3d235b321a180df90c2f58851e92d3b9d226ba9
NHarshwardhan <52920099+NHarshwardhan@users.noreply.github.com>
2023-09-28 02:27:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors')
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors())
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 dotenv.config();
 mongoose.connect("mongodb+srv://math_knots:mathknots@cluster0.sinhd.mongodb.net/mathknots?retryWrites=true&w=majority")
